4074e915bc9dca080bae5d3f8dfef574e553e653
divya15122003 <142145071+divya15122003@users.noreply.github.com>
2023-08-13 14:39:17 +0530

    level2_task4

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-let displayValue = "";
-
-function appendToDisplay(value) {
-  displayValue += value;
-  updateDisplay();
-}
-
-function clearDisplay() {
-  displayValue = "";
-  updateDisplay();
-}
-
-function deleteLast() {
-  displayValue = displayValue.slice(0, -1);
-  updateDisplay();
-}
-
-function calculate() {
-  try {
-    displayValue = eval(displayValue).toString();
-    updateDisplay();
-  } catch (error) {
-    displayValue = "Error";
-    updateDisplay();
+// Dummy user data (for demonstration purposes)
+const users = [
+    { username: 'user1', password: 'password1' },
+    { username: 'user2', password: 'password2' },
+  ];
+  
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+  
+    // Check if the user exists and the password is correct
+    const user = users.find(user => user.username === username && user.password === password);
+  
+    if (user) {
+      showSuccessMessage('Login successful!');
+      // Redirect to the secured page or perform any action you want
+    } else {
+      showErrorMessage('Invalid username or password. Please try again.');
+    }
+  });
+  
+  function showSuccessMessage(message) {
+    const messageDiv = document.getElementById('message');
+    messageDiv.textContent = message;
+    messageDiv.style.color = '#008000';
   }
-}
-
-function updateDisplay() {
-  document.getElementById("display-screen").value = displayValue;
-}
+  
+  function showErrorMessage(message) {
+    const messageDiv = document.getElementById('message');
+    messageDiv.textContent = message;
+    messageDiv.style.color = '#ff0000';
+  }
+  
